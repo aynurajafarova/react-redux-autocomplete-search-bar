e792c451adf894e7c9868fe3e001da2d2bf16e27
aynurajafarova <aynurafj@code.edu.az>
2022-03-06 19:47:36 +0400

    feat(container): create Container component

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import App from "./App";
+import Container from "./client/shared/components/Container/Container";
 import { store } from "./client/shared/redux/store";
 
 import "../src/client/shared/assets/styles/main.css";
 
 ReactDOM.render(
   <Provider {...{ store }}>
-    <App />
+    <Container>
+      <App />
+    </Container>
   </Provider>,
   document.getElementById("root")
 );
